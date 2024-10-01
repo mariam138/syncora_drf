35e2cf06eeab49c5ca167417ab5d0f1ef4dc59ba
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-01 13:05:01 +0000

    install rest framework for api

--- syncora_api/settings.py
+++ syncora_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # DRF framework to create API
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
