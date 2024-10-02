cde440c68d3e930815ffca55102a59460fafe758
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-02 16:30:55 +0000

    feat: override profiles app config with custom signals

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+    # This overrides the ready() method for the app config by registering
+    # the signals created
+    # Code adapted from: https://dev.to/earthcomfy/django-user-profile-3hik
+    def ready(self):
+        import profiles.signals
