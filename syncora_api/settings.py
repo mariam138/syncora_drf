0cebe54aa231b0f3b9fe51c929a705a16e551b61
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-02 15:55:26 +0000

    fix: add local server to allowed hosts for access

--- syncora_api/settings.py
+++ syncora_api/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '8000-mariam138-syncoradrf-2w8pz189o83.ws.codeinstitute-ide.net'
+]
 
 
 # Application definition
