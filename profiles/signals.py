6c806e03fa9a8929e61de010665db4913b33b392
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-02 16:17:20 +0000

    feat: create signals to create profile upon registration

---
+++ profiles/signals.py
@@ -0,0 +1,21 @@
+# Separate folder for creating signals to keep things modular
+# Code for signals to create a Profile instance when a user is created
+# Is adapted from: https://dev.to/earthcomfy/django-user-profile-3hik
+
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+# User model send signal to Profile model to create a profile instance from user instance
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+# Saves the profile instance after creation
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
