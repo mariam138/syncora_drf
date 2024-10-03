46f9125fabd3d294aecad8e69efb238c1ccc5cef
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-03 12:31:55 +0000

    maint: add docstring to profile serialiser

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serialises the data from :model:`profiles.Profile` into JSON format.
+    username, name and email are custom read only fields with the source
+    stemming from the fields in the User model.
+    """
     username = serializers.ReadOnlyField(source="user.username")
     name = serializers.ReadOnlyField(source="user.first_name")
     email = serializers.ReadOnlyField(source="user.email")
