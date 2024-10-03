099eb67012cb4d94ec8b62b76d1de5d33f43754c
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-03 12:53:06 +0000

    maint: add docstring to profile list view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     """
     Serialises the data from :model:`profiles.Profile` into JSON format.
@@ -9,10 +10,19 @@ class ProfileSerializer(serializers.ModelSerializer):
     access as this view is being used for testing purposes only while
     building the API.
     """
+
     username = serializers.ReadOnlyField(source="user.username")
     name = serializers.ReadOnlyField(source="user.first_name")
     email = serializers.ReadOnlyField(source="user.email")
 
     class Meta:
         model = Profile
-        fields = ['id', 'username', 'name', 'email', 'created_on', 'updated_on', 'profile_image']
+        fields = [
+            "id",
+            "username",
+            "name",
+            "email",
+            "created_on",
+            "updated_on",
+            "profile_image",
+        ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
 
 # Create your views here.
 
+
 class ProfileList(generics.ListAPIView):
+    """
+    Displays list of all profiles created. This view will be used for testing
+    purposes only.
+    """
+
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
