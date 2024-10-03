88455a33e45f130ae002885d061bbb336504d912
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-03 12:28:03 +0000

    update: add custom read only fields in serialiser

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source="user.username")
+    name = serializers.ReadOnlyField(source="user.first_name")
+    email = serializers.ReadOnlyField(source="user.email")
+
     class Meta:
         model = Profile
-        fields = ['user', 'created_on', 'updated_on', 'profile_image']
+        fields = ['id', 'username', 'name', 'email', 'created_on', 'updated_on', 'profile_image']
