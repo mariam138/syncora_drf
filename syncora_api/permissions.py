684b6178b0570dae6bac242b46cdbea6d877099a
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-03 16:22:24 +0000

    feat: create custom IsOwnerOrReadOnly permissions class

---
+++ syncora_api/permissions.py
@@ -0,0 +1,21 @@
+# Creates custom permission to check if the user is also the owner of an instance
+# to allow editing, otherwise the view is read-only.
+# Code adapted from the django rest framework documentation
+# https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission which only allows owners of an object to edit it.
+    Otherwise, the object is read-only.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        # and is considered one of the safe methods
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
