1f33c6e566a8d761ba9cdae44ae4098bead01d2b
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-03 11:52:03 +0000

    feat: create generic profile list view + url

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Profile
+from rest_framework import generics
 
 # Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
