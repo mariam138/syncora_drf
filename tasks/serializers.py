e9247c6b08c2e184bab658c635913eadebfb83fb
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-04 14:04:22 +0000

    update: add owner and profile_id read only fields to task serialiser

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="user.username")
+    profile_id = serializers.ReadOnlyField(source="user.profile.id")
+
     class Meta:
         model = Task
         fields = [
+            'id',
+            'owner',
+            'profile_id',
             'title',
             'due_date',
             'priority',
