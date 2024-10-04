d785fb9c90acac22ced6fc1b2878ba28c10024d1
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-04 13:25:19 +0000

    feat: create generic list view for tasks

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 from syncora_api.permissions import IsOwnerOrReadOnly
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Task
+from syncora_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+class TaskList(generics.ListAPIView):
+    """
+    Displays list of tasks created.
+    """
+
+    queryset = Task.objects.all()
