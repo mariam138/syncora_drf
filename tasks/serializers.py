46e6ed83aa954b4df1e5ab11ea8ddfe003029b05
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-06 16:26:02 +0000

    feat: see if task is overdue

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,16 @@
+import datetime
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.user.username")
     is_overdue = serializers.SerializerMethodField()
+
+    def get_is_overdue(self, obj):
+        current_datetime = datetime.datetime.now()
+
+        if current_datetime > obj.due_date:
+            obj.is_overdue = True
 
     class Meta:
         model = Task
@@ -15,5 +22,6 @@ class TaskSerializer(serializers.ModelSerializer):
             'priority',
             'category',
             'description',
+            'is_overdue',
             'completed'
         ]
