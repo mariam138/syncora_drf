16187d83728a491cea34d7706dd4350ab0247323
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-06 17:40:50 +0000

    bug: cannot compare naive and aware datetimes

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,4 +1,6 @@
-import datetime
+from datetime import datetime
+import pytz
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Task
 
@@ -7,10 +9,24 @@ class TaskSerializer(serializers.ModelSerializer):
     is_overdue = serializers.SerializerMethodField()
 
     def get_is_overdue(self, obj):
-        current_datetime = datetime.datetime.now()
+        # current_datetime = datetime.datetime.now()
+        # current_datetime = current_datetime.astimezone(obj.due_date.tzinfo)
 
-        if current_datetime > obj.due_date:
-            obj.is_overdue = True
+        # utc_duedate = obj.due_date.replace(tzinfo=pytz.utc)
+
+        now = timezone.now()
+        # now_1 = now.replace(tzinfo=timezone.utc)
+        due_date = obj.due_date
+        # due_date = due_date.astimezone(tz=None)
+        # now_tz = now.activate()
+
+        print(now)
+        # print('utc now', now_1)
+        print(due_date)
+        # print(current_datetime)
+
+        # if current_datetime > obj.due_date:
+        #     obj.is_overdue = True
 
     class Meta:
         model = Task
