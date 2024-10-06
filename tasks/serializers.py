61bfec5415f7fc0cb77485ad37fda222dbf4d344
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-06 15:43:57 +0000

    fix: get correct model field for owner in task serialiser

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,14 +2,13 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source="profile.user.username")
+    owner = serializers.ReadOnlyField(source="owner.user.username")
 
     class Meta:
         model = Task
         fields = [
             'id',
             'owner',
-            'profile_id',
             'title',
             'due_date',
             'priority',
