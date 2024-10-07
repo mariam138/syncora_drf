d0f495384379682086b7dd3d1514dbd74e5271d1
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-07 12:33:06 +0000

    install django-location-field for use in Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from profiles.models import Profile
 
 # Create your models here.
+
+class Event(models.Model):
+    """
+    Creates an instance of the Event model, which has a many-
+    to-one relationship with :model:`profiles.Profile`.
+    """
