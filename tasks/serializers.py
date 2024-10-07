aec9e215781726ba756729435e5d26e90fda17f6
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-07 07:27:35 +0000

    bug: create mixin to make current datetime aware

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,10 +1,31 @@
 from datetime import datetime
 import pytz
+import zoneinfo
 from django.utils import timezone
 from rest_framework import serializers
 from .models import Task
 
-class TaskSerializer(serializers.ModelSerializer):
+
+class TimezoneMixin:
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+
+        tzname = request.user.time_zone if request.user.is_authenticated else None
+        # if request.user.is_authenticated:
+        #     tzname = request.user.timezone
+        #     # print('Mixin', tzname)
+        if tzname:
+            timezone.activate(zoneinfo.ZoneInfo(tzname))
+        else:
+            timezone.deactivate()
+
+
+class ConfigurableModelSerializer(TimezoneMixin, serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        super(ConfigurableModelSerializer, self).__init__(*args, **kwargs)
+
+
+class TaskSerializer(ConfigurableModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.user.username")
     is_overdue = serializers.SerializerMethodField()
 
@@ -15,14 +36,20 @@ class TaskSerializer(serializers.ModelSerializer):
         # utc_duedate = obj.due_date.replace(tzinfo=pytz.utc)
 
         now = timezone.now()
+        # is_now_aware = now.timezone.is_aware
         # now_1 = now.replace(tzinfo=timezone.utc)
         due_date = obj.due_date
         # due_date = due_date.astimezone(tz=None)
         # now_tz = now.activate()
 
-        print(now)
+        # request = self.context["request"]
+        # tz = request.user.time_zone
+
+        print("Current time:", now)
+        # print(is_now_aware)
         # print('utc now', now_1)
-        print(due_date)
+        print("Due date:", due_date)
+        # print(tz)
         # print(current_datetime)
 
         # if current_datetime > obj.due_date:
@@ -31,13 +58,13 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = [
-            'id',
-            'owner',
-            'title',
-            'due_date',
-            'priority',
-            'category',
-            'description',
-            'is_overdue',
-            'completed'
+            "id",
+            "owner",
+            "title",
+            "due_date",
+            "priority",
+            "category",
+            "description",
+            "is_overdue",
+            "completed",
         ]
