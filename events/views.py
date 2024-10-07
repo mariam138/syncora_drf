dda0b03e6f7a5b81e03c416b0655c87f3f751fb6
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-07 13:33:52 +0000

    feat: create EventList view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Event
+
+from syncora_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+class EventList(generics.ListAPIView):
+    """
+    Displays list of events created.
+    """
+
+    queryset = Event.objects.all()
+    
