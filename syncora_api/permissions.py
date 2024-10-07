f826a4cf9f3c55266286438c35d6606d87ab5390
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-07 08:23:58 +0000

    feat: create newis owner permissions class for task, event and note models

--- syncora_api/permissions.py
+++ syncora_api/permissions.py
@@ -19,3 +19,19 @@ class IsUserOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner of the object
         return obj.user == request.user
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission which only allows owners of an object to edit it.
+    Otherwise, the object is read-only.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        # and is considered one of the safe methods
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the object
+        return obj.owner.user == request.user
