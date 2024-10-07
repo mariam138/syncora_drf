b0a898e1dae641d18598c6ff9f64cf304350dc72
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-07 11:59:52 +0000

    maint: remove commented out and unused code

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,20 +1,10 @@
-from datetime import datetime
-from datetime import timezone as dt_timezone
-import pytz
-import zoneinfo
-from django.utils import timezone
 from rest_framework import serializers
 from .models import Task
 
 
-# class ConfigurableModelSerializer(TimezoneMixin, serializers.ModelSerializer):
-#     def __init__(self, *args, **kwargs):
-#         super(ConfigurableModelSerializer, self).__init__(*args, **kwargs)
-
-
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.user.username")
-    
+
     class Meta:
         model = Task
         fields = [
@@ -28,10 +18,10 @@ class TaskSerializer(serializers.ModelSerializer):
             "completed",
         ]
 
+
 class CreateTaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.user.username")
     completed = serializers.ReadOnlyField(source="task.completed")
-
 
     class Meta:
         model = Task
