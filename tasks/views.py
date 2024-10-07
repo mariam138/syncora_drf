270533b779d08012e977b3079b256a38f232528d
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-07 11:39:09 +0000

    maint: remove timezone mixin code

--- tasks/views.py
+++ tasks/views.py
@@ -1,26 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django.utils import timezone
-import zoneinfo
 from .models import Task
 from .serializers import TaskSerializer, CreateTaskSerializer
 from syncora_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
-
-
-class TimezoneMixin:
-    def initial(self, request, *args, **kwargs):
-        super().initial(request, *args, **kwargs)
-
-        tzname = request.user.timezone if request.user.is_authenticated else None
-        # if request.user.is_authenticated:
-        #     tzname = request.user.timezone
-        print("Mixin", tzname)
-        if tzname:
-            timezone.activate(zoneinfo.ZoneInfo(tzname))
-        else:
-            timezone.deactivate()
 
 
 class TaskList(generics.ListAPIView):
@@ -49,7 +33,7 @@ class CreateTask(generics.CreateAPIView):
         serializer.save(owner=profile)
 
 
-class TaskDetail(TimezoneMixin, generics.RetrieveUpdateDestroyAPIView):
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Will retrieve a task by id and allow it to be viewed in detail.
     Will also allow editing and deletion of the task from one API
