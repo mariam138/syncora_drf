16004364211c0227e29586450bee13850a3359bf
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-08 13:20:58 +0000

    create url for create event api view

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EventList
+from .views import EventList, CreateEvent
 
 urlpatterns = [
     path("events/", EventList.as_view(), name="event_list"),
+    path("events/new/", CreateEvent.as_view(), name="create_event"),
 ]
