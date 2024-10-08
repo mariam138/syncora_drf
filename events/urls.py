332947d6e782e6e96fd92fd5b48f1063319ac3a8
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-08 13:43:15 +0000

    create url for event detail view

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventList, CreateEvent
+from .views import EventList, CreateEvent, EventDetail
 
 urlpatterns = [
     path("events/", EventList.as_view(), name="event_list"),
     path("events/new/", CreateEvent.as_view(), name="create_event"),
+    path("events/<int:pk>/", EventDetail.as_view(), name="event_detail")
 ]
