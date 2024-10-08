f44e25dba24d973cb9b0f4467f5eb9078c3e528a
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-08 15:40:56 +0000

    create url for note detail view

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NoteList, CreateNote
+from .views import NoteList, CreateNote, NoteDetail
 
 urlpatterns = [
     path("notes/", NoteList.as_view(), name="notes_list"),
     path("notes/new/", CreateNote.as_view(), name="create_note"),
+    path("notes/<int:pk>/", NoteDetail.as_view(), name="note_detail"),
 ]
