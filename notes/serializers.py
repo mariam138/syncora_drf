c3ffbc4b8851a2bbef54004a406ebd80925ca4f3
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-08 14:20:43 +0000

    feat: create note serialiser

---
+++ notes/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Note
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    """Serialises instances of the Note model"""
+    owner = serializers.ReadOnlyField(source="owner.user.username")
+
+    class Meta:
+        model = Note
+        fields = ["id", "owner", "title", "date_created", "date_updated", "content"]
