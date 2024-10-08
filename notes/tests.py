a6566e2ddeb43634ece76b13a764b03973da94a4
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-08 16:52:35 +0000

    test: test note list can be retrieved

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Note
 
 # Create your tests here.
+class NoteListViewTests(APITestCase):
+
+    def setUp(self):
+        User.objects.create_user(username='mariam', password="pass")
+    
+    def test_view_list_of_notes(self):
+        mariam = User.objects.get(username='mariam')
+        Note.objects.create(owner=mariam, title='title', content='content')
+        response = self.client.get('/notes/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
