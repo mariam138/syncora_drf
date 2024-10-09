6655423eed49ce2d84c0a2286ee66ced6220c731
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-09 16:11:26 +0000

    test: test that authorised user can create new event

--- events/tests.py
+++ events/tests.py
@@ -33,3 +33,18 @@ class CreateEventViewTests(APITestCase):
     def setUp(self):
         User.objects.create_user(username="mariam", password="pass")
         mariam = Profile.objects.get(id=1)
+
+    def test_authenticated_user_can_create_new_event(self):
+        """Test if a logged in user can create a new event"""
+        self.client.login(username="mariam", password="pass")
+        response = self.client.post("/events/new/", {
+            'owner':'mariam',
+            'name':"test event",
+            'date':"2024-10-09",
+            'start_time':"00:00",
+            'end_time':"00:01",
+            'category':"WORK",
+            'location':"london"
+            })
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
