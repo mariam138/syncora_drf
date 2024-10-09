8836cf60117dbfe7b5655ba04c1bfc2fa29ffa6c
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-09 17:32:13 +0000

    maint: tidy up test code

--- profiles/tests.py
+++ profiles/tests.py
@@ -19,13 +19,13 @@ class ProfileDetailViewTests(APITestCase):
 
     def test_retrieve_profile_by_id(self):
         """Tests that a profile can be retrieved by id"""
-        response = self.client.get('/profiles/1/')
+        response = self.client.get("/profiles/1/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_cannot_retrieve_profile_that_doesnt_exist(self):
         """Tests that a user cannot retrieve a profile that has not been created"""
-        response = self.client.get('/profiles/2410/')
+        response = self.client.get("/profiles/2410/")
 
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
