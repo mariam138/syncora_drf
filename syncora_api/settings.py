aaea85943a8b89d223ac176868e91cd61dd86d49
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-14 14:39:34 +0000

    reinstall dependencies

--- syncora_api/settings.py
+++ syncora_api/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
 ]
 
 # Setting cors headers origins
