999f8181928b084e7232c839ba5a32af697d78fe
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-28 11:43:00 +0000

    update: display human readable category choice label

--- events/serializers.py
+++ events/serializers.py
@@ -4,6 +4,13 @@ from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.user.username")
+    """
+    Display human readable label for category choices. This is used for the
+    front end display. Code adapted from
+    https://github.com/encode/django-rest-framework/issues/1755#issuecomment-945167944
+    and the comment below it.
+    """
+    category_display = serializers.CharField(source="get_category_display")
 
     class Meta:
         model = Event
@@ -15,6 +22,7 @@ class EventSerializer(serializers.ModelSerializer):
             "start_time",
             "end_time",
             "category",
+            "category_display",
             "location",
-            "notes"
+            "notes",
         ]
